fc4e730303cc2af55bf9e884779c14f96330b0e2
Andrew Lopez <andrewlopezcodes@gmail.com>
2024-02-12 14:42:31 -0500

    create changeBackgroundColor function, add event listeners for color pickers

--- backgroundGen.js
+++ backgroundGen.js
@@ -1,1 +1,18 @@
+let bodyItem = document.querySelector("#documentBody");
+let currentCssBackground = document.querySelector("h3");
+let leftColorSelector = document.querySelector(".leftColorPicker");
+let rightColorSelector = document.querySelector(".rightColorPicker");
+
+let changeBackgroundColor = function(){
+  bodyItem.style.background = 
+    "linear-gradient(to right, "
+    + leftColorSelector.value
+    + ", "
+    + rightColorSelector.value
+    + ")";
+};
+
+leftColorSelector.addEventListener("input", changeBackgroundColor);
+
+rightColorSelector.addEventListener("input", changeBackgroundColor);
 
